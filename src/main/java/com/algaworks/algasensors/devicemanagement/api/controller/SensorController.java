501ea7957d85724736caecdc9dd017eaafdf4b52
Edvaldo <edvaldovitor250@gmail.com>
2025-04-18 11:22:48 -0300

    fix(sensor): fix input annotation and TSID generation

---
+++ src/main/java/com/algaworks/algasensors/devicemanagement/api/controller/SensorController.java
@@ -0,0 +1,29 @@
+package com.algaworks.algasensors.devicemanagement.api.controller;
+
+import com.algaworks.algasensors.devicemanagement.api.model.SensorInput;
+import com.algaworks.algasensors.devicemanagement.domain.model.repository.model.Sensor;
+import io.hypersistence.tsid.TSID;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/sensors")
+public class SensorController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Sensor create(@RequestBody SensorInput input){
+        return Sensor.builder()
+                .id(TSID.fast())
+                .name(input.getIp())
+                .location(input.getLocation())
+                .protocol(input.getProtocol())
+                .model(input.getModel())
+                .enabled(false)
+                .build();
+
+    }
+
+}
